a9491c48aeb05f9ff5325eb8fe4fdfe415f4a76f
AsQ <danila.udodov.workmail@gmail.com>
2022-12-05 06:36:06 +0300

    Sending stickers has been done :heavy_check_mark:

--- bot/handler/start.py
+++ bot/handler/start.py
@@ -3,7 +3,8 @@ from start_bot.import_bot import (
     bot,
     ADMIN,
     CONTROL_TABLE,
-    scheduler
+    scheduler,
+    SAFI_REJOICES
 )
 from text import text
 from aiogram import types
@@ -34,8 +35,10 @@ async def start_bot(message: types.Message):
         else:
             pass
     if ADMIN != user_id:
+        await bot.send_sticker(message.from_user.id, SAFI_REJOICES)
         await message.answer(text.start_bot_text_user, reply_markup=manu_keyboard)
     elif ADMIN == user_id:
+        await bot.send_sticker(message.from_user.id, SAFI_REJOICES)
         await message.answer(text.start_bot_text_admin, reply_markup=manu_keyboard_admins)
 
 #everyone, news
@@ -45,60 +48,57 @@ editors_control = "everyone"
 class message_everyone(StatesGroup):
     mailing_list_name = State()
     mailing_list_description = State()
-    mailing_list_sub = State()
     mailing_list_img = State()
+    mailing_list_sub = State()
 async def message_everyone_add(message: types.Message):
   if message.from_user.id == ADMIN:
-    await message.reply('💻Напишите название рассылки', reply_markup=keyboard_for_exiting_the_state_machine)
+    await message.reply(text=text.message_everyone_text_state_add, reply_markup=keyboard_for_exiting_the_state_machine)
     await message_everyone.mailing_list_name.set()
   else:
-    await message.answer('Я не понимаю твои слова. Напиши /start или /help')
+    await message.answer(text=text.error_message)
 #ловим название
 async def name_message_everyone_bot(message: types.Message, state: FSMContext):
   async with state.proxy() as data:
     data['mailing_list_name'] = message.text
   await message_everyone.next()
-  await message.answer('💻Напишите текст рассылки')
+  await message.answer(text=text.message_everyone_text_state_name)
 #ловим описание
 async def description_message_everyone_bot(message: types.Message, state: FSMContext):
   async with state.proxy() as data:
     data['mailing_list_description'] = message.text
   await message_everyone.next()
-  await message.answer('💻 Подпишитесь')
+  await message.answer(text=text.message_everyone_text_state_photo)
+
+async def photo_message_everyone_bot(message: types.Message, state: FSMContext):
+	async with state.proxy() as data:
+		data['mailing_list_img'] = message.photo[0].file_id
+	await message.answer(text=text.message_everyone_text_state_sub)
+	await message_everyone.next()
+
 #ловим подпись
 async def dev_message_everyone_bot(message: types.Message, state: FSMContext):
     global data_global_everyone
     async with state.proxy() as data:
         data['mailing_list_sub'] = message.text
-        data['mailing_list_img'] = "https://www.ampersand.net/wp-content/uploads/2018/06/quotationew.jpg"
-    await message.answer('☑️ рассылка отправлена', reply_markup=manu_keyboard_admins)
+    await message.answer(text=text.message_everyone_text_state_send, reply_markup=manu_keyboard_admins)
     data_global_everyone = await state.get_data()
     await check_error(message)
     await state.finish()
+
 async def check_error(message: types.Message):
     global editors_control
     editors_control = "everyone"
     mailing_list_name = str(data_global_everyone.get("mailing_list_name"))
-    mailing_list_img = str(data_global_everyone.get("mailing_list_img"))
+    mailing_list_img = data_global_everyone.get("mailing_list_img")
     mailing_list_description = str(data_global_everyone.get("mailing_list_description"))
     mailing_list_sub = str(data_global_everyone.get("mailing_list_sub"))
     await bot.send_photo(message.from_user.id, mailing_list_img, f'{mailing_list_name}\n{mailing_list_description}\n{mailing_list_sub}',\
         reply_markup=InlineKeyboardMarkup(resize_keyboard=True)\
-            .add(InlineKeyboardButton(f"{text.edit_mailing_list_name}", callback_data=f"name {mailing_list_name}"))\
-            .add(InlineKeyboardButton(f"{text.edit_mailing_list_description}", callback_data=f"description {mailing_list_description}"))\
-            .add(InlineKeyboardButton(f"{text.edit_mailing_list_sub}", callback_data=f"sub {mailing_list_sub}"))\
+            .add(InlineKeyboardButton(f"{text.edit_mailing_list_name}", callback_data=f"name handler_"))\
+            .add(InlineKeyboardButton(f"{text.edit_mailing_list_description}", callback_data=f"description handler_"))\
+            .add(InlineKeyboardButton(f"{text.edit_mailing_list_sub}", callback_data=f"sub handler_"))\
             .add(InlineKeyboardButton(f"{text.delete_mailing_list}", callback_data=f"delete_mailing_list_"))\
             .insert(InlineKeyboardButton(f"{text.send_mailing_list}", callback_data="send_mailing_list_")))
-
-
-
-
-
-
-
-
-
-
 
 #машина состояний
 class message_news(StatesGroup):
@@ -108,29 +108,29 @@ class message_news(StatesGroup):
     news_list_img = State()
 async def message_news_add(message: types.Message):
   if message.from_user.id == ADMIN:
-    await message.reply('💻Напишите название новости', reply_markup=keyboard_for_exiting_the_state_machine)
+    await message.reply(text=text.message_news_text_state_add, reply_markup=keyboard_for_exiting_the_state_machine)
     await message_news.news_list_name.set()
   else:
-    await message.answer('Я не понимаю твои слова. Напиши /start или /help')
+    await message.answer(text=text.error_message)
 #ловим название
 async def name_news_everyone_bot(message: types.Message, state: FSMContext):
   async with state.proxy() as data:
     data['news_list_name'] = message.text
   await message_news.next()
-  await message.answer('💻Напишите текст новости')
+  await message.answer(text=text.message_news_text_state_name)
 #ловим описание
 async def description_message_news_bot(message: types.Message, state: FSMContext):
   async with state.proxy() as data:
     data['news_list_description'] = message.text
   await message_news.next()
-  await message.answer('💻Подпишитесь')
+  await message.answer(text=text.message_news_text_state_sub)
 #ловим подпись
 async def dev_message_news_bot(message: types.Message, state: FSMContext):
     global data_global_news
     async with state.proxy() as data:
         data['news_list_sub'] = message.text
         data['news_list_img'] = "https://www.ampersand.net/wp-content/uploads/2018/06/quotationew.jpg"
-    await message.answer('☑️ рассылка отправлена', reply_markup=manu_keyboard_admins)
+    await message.answer(text=text.message_news_text_state_send, reply_markup=manu_keyboard_admins)
     data_global_news = await state.get_data()
     await check_error_file(message)
     await state.finish()
@@ -353,6 +353,7 @@ def reg_handler(dp):
     dp.register_message_handler(message_everyone_add, text='Сделать рассылку', state=None)
     dp.register_message_handler(name_message_everyone_bot, state=message_everyone.mailing_list_name)
     dp.register_message_handler(description_message_everyone_bot, state=message_everyone.mailing_list_description)
+    dp.register_message_handler(photo_message_everyone_bot, content_types=['photo'], state=message_everyone.mailing_list_img)
     dp.register_message_handler(dev_message_everyone_bot, state=message_everyone.mailing_list_sub)
 
     dp.register_message_handler(finish_state_name_update, state=update_user_name.mailing_list_name)
